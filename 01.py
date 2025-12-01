28803448e8df306bb777aa56acf0c3d384ebbf8a
Michael Fogleman <fogleman@gmail.com>
2025-12-01 08:37:18 -0500

    golf

--- 01.py
+++ 01.py
@@ -1,21 +1,11 @@
 import fileinput
 
-p1 = 0
-p2 = 0
-x = 50
-
+p1, p2, x = 0, 0, 50
 for line in fileinput.input():
-    d = line[0]
-    n = int(line[1:])
-    for i in range(n):
-        if d == 'L':
-            x = (x - 1) % 100
-        else:
-            x = (x + 1) % 100
-        if x == 0:
-            p2 += 1
-    if x == 0:
-        p1 += 1
+    for i in range(int(line[1:])):
+        x = (x + [-1, 1][line[0] == 'R']) % 100
+        p2 += x == 0
+    p1 += x == 0
 
 print(p1)
 print(p2)

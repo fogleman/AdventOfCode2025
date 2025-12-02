66dc93080ed254740056353d4f271de656df6a64
Michael Fogleman <fogleman@gmail.com>
2025-12-02 16:10:38 -0500

    cleanup

--- 02.py
+++ 02.py
@@ -6,15 +6,9 @@ numbers = list(map(int, re.findall(r'\d+', next(fileinput.input()))))
 p1 = p2 = 0
 for a, b in zip(numbers[::2], numbers[1::2]):
     for x in range(a, b + 1):
-        s = str(x)
-        n = len(s)
-        i = n // 2
-        if s[:i] == s[i:]:
-            p1 += x
-        for k in range(1, i + 1):
-            if all(s[:k] == s[j:j+k] for j in range(0, n, k)):
-                p2 += x
-                break
+        s = str(x); n = len(s)
+        p1 += x * (s[:n // 2] == s[n // 2:])
+        p2 += x * any(all(s[:k] == s[i:i+k] for i in range(k, n, k)) for k in range(1, n))
 
 print(p1)
 print(p2)

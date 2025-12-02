1da2df472046fb89e78f6349069f5ba22c85527c
Michael Fogleman <fogleman@gmail.com>
2025-12-02 16:26:11 -0500

    cool trick

--- 02.py
+++ 02.py
@@ -6,9 +6,9 @@ numbers = list(map(int, re.findall(r'\d+', next(fileinput.input()))))
 p1 = p2 = 0
 for a, b in zip(numbers[::2], numbers[1::2]):
     for x in range(a, b + 1):
-        s = str(x); n = len(s)
-        p1 += x * (s[:n // 2] == s[n // 2:])
-        p2 += x * any(all(s[:k] == s[i:i+k] for i in range(k, n, k)) for k in range(1, n))
+        s = str(x); n = len(s); i = n // 2
+        p1 += x * (s[:i] == s[i:])
+        p2 += x * (s in (s + s)[1:-1])
 
 print(p1)
 print(p2)
